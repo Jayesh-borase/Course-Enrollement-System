99c08d4ce6ff8696348252b39aa8eb5621e0707a
jayesh-borase <borsejayesh069@gmail.com>
2025-12-24 22:28:28 +0530

    added user route

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const cors=require("cors");
 const dotenv=require("dotenv");
 const connectDb=require("./config/db.js");
-const Student=require("./models/Student.js");
+
 
 dotenv.config();
 connectDb();
@@ -12,9 +12,14 @@ const app=express();
 app.use(express.json());
 app.use(cors());
 
-const stud=new Student({phone:"9325525258",email:"jayeshb@gmail.com",password:"Jayeshb@18"});
-stud.save();
+// const stud=new Student({phone:"9325525258",email:"jayeshb@gmail.com",password:"Jayeshb@18"});
+// stud.save();
 
+const authRoute=require("./Routes/authRoutes.js");
+app.use("/auth",authRoute);
+
+const userRoute=require("./Routes/userRoute.js");
+app.use("/api",userRoute);
 
 app.listen(5000,() =>{
     console.log("server is running..");
